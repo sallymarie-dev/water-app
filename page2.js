e602ad52c4de60163daae7eaf0dc9ed538e2bfb6
Sally <causeysally@gmail.com>
2025-09-03 21:52:29 -0500

    button update

--- page2.js
+++ page2.js
@@ -1,25 +1,28 @@
 console.log("hello test");
-setProperty("waterAppHeader", "color", "green");
+setProperty("header", "color", "green");
 
 setProperty("waterbottle", "background-color", "red");
 
 onEvent("waterbottle", "click", change);
+function change(){
+  console.log("hello change")
+}
+onEvent("english1", "click", translatingToEnglish);
 
-onEvent("english", "click", translatingToEnglish);
-
-onEvent("spanish", "click", translatingToSpanish);
+onEvent("spanish1", "click", translatingToSpanish);
 
 function translatingToEnglish() {
-  setText("waterAppHeader", "Water Conservation Tips");
-  setText("spanish", "Español");
+  console.log("hello english");
+  setText("header", "Water Conservation Tips");
+  setText("spanish1", "Español");
   setText(
     "subtext",
     "Instead of buying disposable water bottles,use a refillable bottle. One use water bottles fill our landfills and trap valuable water."
   );
 }
 function translatingToSpanish() {
-  setText("waterAppHeader", "Consejos para ahorrar Agua");
-  setText("english", "English");
+  setText("header", "Consejos para ahorrar Agua");
+  setText("english1", "English");
   setText(
     "subtext",
     "En lugar de comprar botellas de agua desechables, usa una botella rellenable. Las botellas de un solo uso llenan los vertederos y retienen agua valiosa."
